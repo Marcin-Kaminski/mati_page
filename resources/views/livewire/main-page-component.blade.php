d65843f2adbb6cfba2df76c227b6b55eddd6a6c2
Marcin Kamiński <sz3lejesz2@gmail.com>
2024-01-23 14:01:52 +0100

    poprawki

--- resources/views/livewire/main-page-component.blade.php
+++ resources/views/livewire/main-page-component.blade.php
@@ -15,16 +15,35 @@
             <div class="separating-stripe"></div>
         </div>
         <div class="mid-box">
-            <div class="centered-text" style="font-size: 2.8rem">CO OFERUJE?</div>
+            <div class="centered-text rem28 mr15" >CO OFERUJE?</div>
             <div class="mix-and-master-box">
-                <div class="white-box">
 
+                <div class="mixer-white-box">
+                    <div class="white-box float-left">
+                        <img src="images/mixer-logo.png" alt="" class="mixer">
+                        <div class="centered-text black-color mixer-title">Miks</div>
+                        <div class="centered-text rem-15 black-color mix-description-text">Miks to proces łaczenia sciezek audio, dostosowywania głosnosci i dodawania efektów, aby uzyskac spójne brzmienie.</div>
+                        <div class="centered-text rem-20 black-color mb-15">Czas realizacji 48h</div>
+                    </div>
                 </div>
-                <div class="white-box">
-                    <img src="images/mixer-logo.png" alt="" class="mixer">
-                    <div class="centered-text; rem-15; black-color">Miks</div>
+
+                <div class="master-white-box">
+                    <div class="white-box float-right">
+                        <img src="images/headphones-icon.png" alt="" class="headphones">
+                        <div class="centered-text black-color mixer-title">Mastering</div>
+                        <div class="centered-text rem-15 black-color mix-description-text">Mastering to ostateczna obróbka dzwieku po miksie, celem jest poprawa ogólnego brzmienia, dynamiki i dostosowanie do róznych nosników.</div>
+                        <div class="centered-text rem-20 black-color mb-15">Czas realizacji 48h</div>
+                    </div>
                 </div>
+                <div class="clearfix"></div>
+                <div class="mix-price float-left">CENA: 50zł</div>
+                <div class="master-price float-right">CENA: 30zł</div>
+                <div class="clearfix"></div>
             </div>
+            <div class="centered-text ">MIX&MASTER</div>
+            <div class="centered-text ">70ZŁ</div>
+            <div class="clearfix"></div>
+            dd15
         </div>
     </div>
 
