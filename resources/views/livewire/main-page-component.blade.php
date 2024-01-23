5007d41cfafb840813b9deaf19230de709d737db
Marcin Kamiński <sz3lejesz2@gmail.com>
2024-01-23 16:00:25 +0100

    prace

--- resources/views/livewire/main-page-component.blade.php
+++ resources/views/livewire/main-page-component.blade.php
@@ -7,17 +7,16 @@
             <div class="violin-key-box">
                 <img src="images/portfolio-violin-key.png" wire:click="goToPortfolio" alt="sfx" class="portfolio-violin-key">
             </div>
-            <div class="site-title">mattmastering.pl</div>
+            <div class="site-title mb-3p">mattmastering.pl</div>
             <div class="about-me-box">
                 <img src="images/about-me-book.png" wire:click="goToAboutMe" alt="sfx" class="about-me-book">
             </div>
             <div class="clearfix"></div>
-            <div class="separating-stripe"></div>
+            <div class="separating-stripe mb-3p"></div>
         </div>
         <div class="mid-box">
             <div class="centered-text rem28 mr15" >CO OFERUJE?</div>
             <div class="mix-and-master-box">
-
                 <div class="mixer-white-box">
                     <div class="white-box float-left">
                         <img src="images/mixer-logo.png" alt="" class="mixer">
@@ -26,7 +25,6 @@
                         <div class="centered-text rem-20 black-color mb-15">Czas realizacji 48h</div>
                     </div>
                 </div>
-
                 <div class="master-white-box">
                     <div class="white-box float-right">
                         <img src="images/headphones-icon.png" alt="" class="headphones">
@@ -36,14 +34,29 @@
                     </div>
                 </div>
                 <div class="clearfix"></div>
-                <div class="mix-price float-left">CENA: 50zł</div>
-                <div class="master-price float-right">CENA: 30zł</div>
+                <div class="mix-price float-left rem35">CENA: 50zł</div>
+                <div class="master-price float-right rem35">CENA: 30zł</div>
                 <div class="clearfix"></div>
             </div>
-            <div class="centered-text ">MIX&MASTER</div>
-            <div class="centered-text ">70ZŁ</div>
-            <div class="clearfix"></div>
-            dd15
+            <div class="centered-text rem35">MIX&MASTER</div>
+            <div class="centered-text rem35 mb-3p">70ZŁ</div>
+            <div class="separating-stripe mb-3p"></div>
+            <div class="centered-text rem35 mb-3p">O MNIE</div>
+            <div class="centered-text rem28 introduction">Siema, jestem Mati, mixuje juz 4 rok, zajawkowo robie muze. Z moja wiedza, i Twoja włozona praca w numer dojdziemy do perfekcji i stworzymy cos mocnego!</div>
+            <div class="separating-stripe mb-3p"></div>
+            <div class="centered-text rem28 mb-3p">MIKS PRZED I PO</div>
+            <div class="before-miks-box">
+                <div class="centered-text rem28 mb-3p">PRZED</div>
+                <div class="sfxtest123"></div>
+                <div class="centered-text  mb-3p artist-name">KSYWA ARTYSTY</div>
+            </div>
+            <div class="after-miks-box">
+                <div class="centered-text rem28 mb-3p">PO</div>
+                <div class="sfxtest123"></div>
+                <div class="centered-text  mb-3p artist-name">KSYWA ARTYSTY</div>
+            </div>
+
+
         </div>
     </div>
 
