52adb4c37dfc1ff405677f276fa0c4cca1478686
Marcin Kamiński <sz3lejesz2@gmail.com>
2024-02-21 18:38:14 +0100

    @media queriees

--- resources/views/livewire/main-page-component.blade.php
+++ resources/views/livewire/main-page-component.blade.php
@@ -100,7 +100,8 @@
                 <img src="images/instagram-logo.png" class="ig-logo" alt="MissingImage">
                 <img src="images/youtube-logo.png" class="yt-logo" alt="MissingImage">
                 <div class="centered-text foot-name">© 2024 mattmastering.pl</div>
-                <div class="designer" >Designed by @schizofreniaparanoidalna200</div>
+                <div class="designer">Designed by @schizofreniaparanoidalna200</div>
+                <div class="check-playlist" style="display: none">SPRAWDZ PLAYLISTE NA YT!</div>
             </div>
         </div>
     </div>
